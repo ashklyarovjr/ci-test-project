445357abd5700182951239e66e4f3a34de6976f9
Anton_Shkliarov <Anton_Shkliarov@epam.com>
2015-10-13 15:09:59 +0300

    Initial commit

---
+++ src/main/java/utils/PathsKeeper.java
@@ -0,0 +1,8 @@
+package utils;
+
+
+public class PathsKeeper {
+    public static final String INPUT_FILE_URL = "https://dl.dropboxusercontent.com/u/98396761/NewTxt.txt";
+    public static final String OUTPUT_FILE_PATH_NIO = System.getProperty("user.dir") + "\\src\\files\\downloadNIO.txt";
+    public static final String OUTPUT_FILE_PATH_FUTILS = System.getProperty("user.dir") + "\\src\\files\\downloadFileUtils.txt";
+}
