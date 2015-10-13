99c11b5662278fda6653cce9cf3c1e6aacec81fe
Anton_Shkliarov <Anton_Shkliarov@epam.com>
2015-10-13 19:02:52 +0300

    gitignore added and build commands

--- src/main/java/MainDownload.java
+++ src/main/java/MainDownload.java
@@ -3,7 +3,7 @@ import utils.PathsKeeper;
 
 import java.io.IOException;
 
-public class Main {
+public class MainDownload {
     public static void main(String[] args) throws IOException {
         FileDownloader.downloadAFileFromNetWithNio(PathsKeeper.INPUT_FILE_URL, PathsKeeper.OUTPUT_FILE_PATH_NIO);
         System.out.println("Please, check " + PathsKeeper.OUTPUT_FILE_PATH_NIO + " file");
