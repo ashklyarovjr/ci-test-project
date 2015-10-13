3fd3af15280dc0342c1a2a21cf34371bb7b9f19d
Anton_Shkliarov <Anton_Shkliarov@epam.com>
2015-10-13 19:18:49 +0300

    paths update

--- src/main/java/utils/PathsKeeper.java
+++ src/main/java/utils/PathsKeeper.java
@@ -3,6 +3,6 @@ package utils;
 
 public class PathsKeeper {
     public static final String INPUT_FILE_URL = "https://dl.dropboxusercontent.com/u/98396761/NewTxt.txt";
-    public static final String OUTPUT_FILE_PATH_NIO = System.getProperty("user.dir") + "\\src\\files\\downloadNIO.txt";
-    public static final String OUTPUT_FILE_PATH_FUTILS = System.getProperty("user.dir") + "\\src\\files\\downloadFileUtils.txt";
+    public static final String OUTPUT_FILE_PATH_NIO = "C:\\jenkins\\downloadNIO.txt";
+    public static final String OUTPUT_FILE_PATH_FUTILS = "C:\\jenkins\\downloadFileUtils.txt";
 }
